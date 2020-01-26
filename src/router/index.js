04c50aa9034a7847e8492ba0981691dc9632cb19
nargok <nargok@gmail.com>
2020-01-26 13:34:24 +0900

    router settings

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,26 @@ const routes = [
     component: Home
   },
   {
+    // path: '/search',
+    // name: 'search',
+    // component: BookSearch
+  },
+  {
+    // path: '/form',
+    // name: 'form',
+    // component: BookForm
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    redirect: '/'
   }
 ]
 
